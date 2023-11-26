71aa47bc7637e5853c8f1047d9eba64e902d4158
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-11-26 11:40:21 -0400

    Update 643.js

--- 643.js
+++ 643.js
@@ -1,30 +1,21 @@
-
-
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    let sum = nums[0];
-    let maxAvg = -Infinity;
+    let sum = 0;
     let i = 0;
-    let j = 1;
-    while (j < nums.length + 1) {
-        const length = j - i;
-        if (length < k) {
-            sum += nums[j];
-            j++;
-            continue;
-        }
-        const avg = sum / length;
-        if (avg !== Infinity && maxAvg < avg) {
-            maxAvg = avg;
-        }
-        if (length === k) {
-            sum -= nums[i];
-            i++;
-        }
+    while (i < k) {
+        sum += nums[i];
+        i++;
     }
-    return maxAvg;
+    let maxSum = sum;
+    while (i < nums.length) {
+        sum -= nums[i - k];
+        sum += nums[i];
+        maxSum = Math.max(maxSum, sum);
+        i++;
+    }
+    return maxSum / k;
 };
