97c8add650c49ca9c6736bfa177da7d037390ebc
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-10 15:29:25 -0400

    Update 72.js

--- 72.js
+++ 72.js
@@ -1,3 +1,4 @@
+// Time complexity: O(mn * m), Space complexity: O(mn * m)
 /**
  * @param {string} word1
  * @param {string} word2
@@ -30,3 +31,26 @@ var minDistance = function(word1, word2, i = 0, j = 0, memo = {}) {
     );
     return memo[word1][word2];
 };
+
+
+// Time complexity: O(mn), Space complexity: O(mn + m + n) = O(mn)
+// var minDistance = function(word1, word2, i = word1.length, j = word2.length, memo = {}) {
+//     if (!(i in memo)) memo[i] = {};
+//     if (i <= 0) return j;
+//     if (j <= 0) return i;
+//     if (i in memo && j in memo[i]) return memo[i][j];
+//     // continue if characters are the same
+//     if (word1[i - 1] === word2[j - 1]) memo[i][j] = minDistance(word1, word2, i - 1, j - 1, memo);
+//     else {
+//         memo[i][j] = Math.min(
+//             // insert a character
+//             1 + minDistance(word1, word2, i, j - 1, memo),
+//             // delete a character
+//             1 + minDistance(word1, word2, i - 1, j, memo),
+//             // replace a character
+//             1 + minDistance(word1, word2, i - 1, j - 1, memo),
+//         );
+//     }
+//     return memo[i][j];
+// };
+
