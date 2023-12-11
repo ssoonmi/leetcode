364cffb774182228117f5c8d89511750d043029a
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-10 21:55:41 -0400

    Update 1268.js

--- 1268.js
+++ 1268.js
@@ -5,36 +5,25 @@
  */
 var suggestedProducts = function(products, searchWord) {
     const trie = {};
-    products = products.sort().reverse();
+    products = products.sort();
     for (const product of products) {
         let node = trie;
         for (const char of product) {
             if (!(char in node)) node[char] = {};
+            if (!('words' in node[char])) node[char].words = [];
+            node[char].words.push(product);
             node = node[char];
         }
-        node.isWord = true;
     }
     let searches = [];
     let node = trie;
-    let prefix = '';
     for (const char of searchWord) {
-        const search = [];
         if (!node || !(char in node)) {
-            searches.push(search);
+            searches.push([]);
             node = null;
             continue;
         }
-        prefix += char;
-        const stack = [[node[char], prefix]];
-        while (stack.length && search.length < 3) {
-            const [node, prefix] = stack.pop();
-            if (node.isWord) search.push(prefix);
-            for (const char in node) {
-                if (char === 'isWord') continue;
-                stack.push([node[char], prefix + char]);
-            }
-        }
-        searches.push(search);
+        searches.push(node[char].words.slice(0, 3));
         node = node[char];
     }
     return searches;
