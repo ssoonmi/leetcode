e777d060c3f96120da1c036dacbc8e1c225c73d3
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-10 21:30:12 -0400

    Create 208.js

---
+++ 208.js
@@ -0,0 +1,51 @@
+
+var Trie = function() {
+    this.root = {};
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let node = this.root;
+    for (const char of word) {
+        if (!(char in node)) node[char] = {};
+        node = node[char];
+    }
+    node.isWord = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let node = this.root;
+    for (const char of word) {
+        if (char in node) node = node[char];
+        else return false;
+    }
+    return !!node.isWord;
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let node = this.root;
+    for (const char of prefix) {
+        if (char in node) node = node[char];
+        else return false;
+    }
+    return true;
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
