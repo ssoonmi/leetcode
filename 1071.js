5245805356d58b498ca8fbe556db97de1202e61f
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-11 23:14:41 -0400

    Update 1071.js

--- 1071.js
+++ 1071.js
@@ -4,24 +4,15 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    let maxLength = Math.max(str1.length, str2.length);
-    let prefix = Math.min(str1.length, str2.length) === str1.length ? str1 : str2;
-    let prefixMaxLength = prefix.length;
-    while (prefixMaxLength >= 1) {
-        while (str1.length % prefixMaxLength !== 0 || str2.length % prefixMaxLength !== 0) {
-            prefixMaxLength--;
-        }
-        let i = 0;
-        while (i < maxLength) {
-            if (
-                (i >= str1.length || prefix[i % prefixMaxLength] === str1[i]) &&
-                (i >= str2.length || prefix[i % prefixMaxLength] === str2[i])
-            ) {
-                i++;
-            } else break;
-            if (i === maxLength) return prefix.slice(0, prefixMaxLength);
-        }
-        prefixMaxLength--;
-    }
-    return '';
+    if (str1 + str2 !== str2 + str1) return '';
+    if (str2.length === 0) return str1;
+    if (str1.length === 0) return str2;
+    return str2.substring(0, greatestCommonDenominator(str1.length, str2.length));
+};
+
+
+// algorithm to find the greatest common denominator:
+const greatestCommonDenominator = (a, b) => {
+    if (b === 0) return a;
+    return greatestCommonDenominator(b, a % b);
 };
