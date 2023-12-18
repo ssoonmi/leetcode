dfdf4f9bc91175a0bae36f6c9da3d2cc879b8112
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-18 11:31:04 -0400

    Update 1372.js

--- 1372.js
+++ 1372.js
@@ -1,5 +1,22 @@
-// Old Case 2: [1,1,1,null,1,null,null,1,1,null,1]
+/*
+        0
+       / \
+      1   4
+     /     \
+    3       2
+           / \
+          5   6
 
+0 null  => 0 + 3
+1 true  => 1 + 1 + 1
+2 false => 1 + 1
+5 true  => 1
+
+0 null  => 0
+4 false => 0 + 2
+2 false => 1 + 1
+5 true  => 1
+*/
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -12,80 +29,16 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const dfs = function(root, wasLeft, streak) {
-    if (!root) return streak;
-    if (wasLeft) {
-        return Math.max(
-            dfs(root.left, true, 0),
-            dfs(root.right, false, streak + 1)
-        );
-    } else {
-        return Math.max(
-            dfs(root.right, false, 0),
-            dfs(root.left, true, streak + 1)
-        );
-    }
-}
-// var longestZigZag = function(root) {
-var longestZigZag = function(root, wasLeft = null, streak = 0) {
-    // let longest = 0;
-    // const stack = [{ node: root, streak: 0, wasLeft: null }];
-    // while (stack.length) {
-    //     const { node, streak, wasLeft } = stack.pop();
-    //     if (node) {
-    //         longest = Math.max(longest, streak);
-    //         stack.push({
-    //             node: node.right,
-    //             streak: wasLeft === true ? streak + 1 : 1,
-    //             wasLeft: false,
-    //         });
-    //         stack.push({
-    //             node: node.left,
-    //             streak: wasLeft === false ? streak + 1 : 1,
-    //             wasLeft: true,
-    //         });
-    //     }
-    // }
-    // return longest;
-    // return Math.max(
-    //     dfs(root.left, true, 0),
-    //     dfs(root.right, false, 0)
-    // )
-    if (!root) return streak;
-    if (wasLeft === null) {
+var longestZigZag = function(root, isLeft = null, maxZigZag = 0) {
+    if (!root) return maxZigZag;
+    if (isLeft === null) {
         return Math.max(
             longestZigZag(root.left, true, 0),
             longestZigZag(root.right, false, 0)
         );
     }
-    if (wasLeft) {
-        return Math.max(
-            longestZigZag(root.right, false, streak + 1),    
-            longestZigZag(root.left, true, 0)
-        );
-    } else {
-        return Math.max(
-            longestZigZag(root.left, true, streak + 1),
-            longestZigZag(root.right, false, 0)
-        );
-    }
-    if (wasLeft === null || wasLeft === true) {
-        let leftLongest = 0;
-        if (wasLeft === true) {
-            leftLongest++;
-        }
-        leftLongest += longestZigZag(root.right, false);
-        longest = Math.max(longest, leftLongest);
-    }
-    if (wasLeft === null || wasLeft === false) {
-        let rightLongest = 0;
-        if (wasLeft === false) {
-            rightLongest++;
-        }
-        rightLongest += longestZigZag(root.left, true);
-        longest = Math.max(longest, rightLongest);
-    }
-    longest = Math.max(longest, longestZigZag(root.left, null));
-    longest = Math.max(longest, longestZigZag(root.right, null));
-    return longest;
+    return Math.max(
+        longestZigZag(root.left, true, isLeft ? 0 : maxZigZag + 1),
+        longestZigZag(root.right, false, isLeft ? maxZigZag + 1 : 0)
+    );
 };
