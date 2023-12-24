efcc61ce6bb436dbe987f6b63058a02a373e2952
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-24 18:01:20 -0400

    Update 1926.js

--- 1926.js
+++ 1926.js
@@ -1,31 +1,37 @@
+const DIRS = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+];
+
 /**
  * @param {character[][]} maze
  * @param {number[]} entrance
  * @return {number}
  */
 var nearestExit = function(maze, entrance) {
-    entrance.push(0);
+    entrance.push(0)
     const queue = [entrance];
-    const minRow = 0;
-    const maxRow = maze.length - 1;
-    const minCol = 0;
-    const maxCol = maze[0].length - 1;
-    const diff = [[-1, 0], [1, 0], [0, -1], [0, 1]];
     while (queue.length) {
         const [row, col, steps] = queue.shift();
         if (
             steps !== 0 &&
-            (
-                row === minRow || row === maxRow ||
-                col === minCol || col === maxCol
-            )
-        ) return steps;
-        if (maze[row][col] === '*') continue;
-        maze[row][col] = '*';
-        for (const [x, y] of diff) {
-            if (maze[row + x]?.[col + y] === '.') {
-                queue.push([row + x, col + y, steps + 1]);
+            (row === 0 || row === maze.length - 1 ||
+            col === 0 || col === maze[0].length - 1)
+        ) {
+            return steps;
+        }
+        if (maze[row][col] !== '.') continue;
+        maze[row][col] = 'V';
+        for (const [rowD, colD] of DIRS) {
+            const newRow = row + rowD;
+            const newCol = col + colD;
+            if (!(newRow in maze) || !(newCol in maze[newRow])) {
+                continue;
             }
+            if (maze[newRow][newCol] !== '.') continue;
+            queue.push([newRow, newCol, steps + 1]);
         }
     }
     return -1;
