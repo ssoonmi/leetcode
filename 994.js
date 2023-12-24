9619a97b631f1293e7756652374e81fde5b7ebf5
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-24 18:14:12 -0400

    Update 994.js

--- 994.js
+++ 994.js
@@ -1,29 +1,41 @@
+const DIRS = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+];
+
 /**
  * @param {number[][]} grid
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    let numOranges = grid.reduce((sum, row) => sum + row.reduce((sum, col) => (col === 1 || col === 2) ? sum + 1 : sum, 0), 0);
-    let maxMins = 0;
     const queue = [];
+    let numFreshOranges = 0;
     for (const row in grid) {
         for (const col in grid[row]) {
-            if (grid[row][col] === 2) queue.push([Number(row), Number(col), 0]);
-        }
-    }
-    const dirs = [[1, 0],[-1, 0],[0, 1],[0, -1]];
-    while (queue.length) {
-        const [row, col, mins] = queue.shift();
-        if (grid[row][col] === 0) continue;
-        grid[row][col] = 0;
-        maxMins = Math.max(maxMins, mins);
-        numOranges--;
-        for (const [x, y] of dirs) {
-            if (grid[row + x]?.[col + y] === 1) {
-                queue.push([row + x, col + y, mins + 1]);
+            if (grid[row][col] === 2) {
+                queue.push([Number(row), Number(col), 0]);
+                numFreshOranges++;
+            } else if (grid[row][col] === 1) {
+                numFreshOranges++;
             }
         }
     }
-    if (numOranges !== 0) return -1;
-    return maxMins;
+    let maxMins = 0;
+    while (queue.length) {
+        const [row, col, mins] = queue.shift();
+        if (mins !== 0 && grid[row][col] !== 1) continue;
+        grid[row][col] = 2;
+        numFreshOranges--;
+        maxMins = mins;
+        for (const [rowD, colD] of DIRS) {
+            const newRow = rowD + row;
+            const newCol = colD + col;
+            if (!(newRow in grid) || !(newCol in grid[newRow])) continue;
+            if (grid[newRow][newCol] !== 1) continue;
+            queue.push([newRow, newCol, mins + 1]);
+        }
+    }
+    return numFreshOranges === 0 ? maxMins : -1;
 };
