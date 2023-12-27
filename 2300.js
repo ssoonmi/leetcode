90708a42e98016d1167e3132577a148d4f733d40
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-27 18:37:55 -0400

    Update 2300.js

--- 2300.js
+++ 2300.js
@@ -5,21 +5,20 @@
  * @return {number[]}
  */
 var successfulPairs = function(spells, potions, success) {
-    potions = potions.sort((a, b) => a - b);
-    for (const idx in spells) {
-        const spell = spells[idx];
+    potions.sort((a, b) => a - b);
+    return spells.map(spell => {
         let i = 0;
         let j = potions.length;
         while (i < j) {
             const mid = Math.floor((j - i) / 2) + i;
-            const potion = potions[mid];
-            if (potion * spell < success) {
+            const strength = potions[mid] * spell;
+            if (strength < success) {
                 i = mid + 1;
             } else {
                 j = mid;
             }
         }
-        spells[idx] = potions.length - i;
-    }
-    return spells;
+        if (i >= j) return potions.length - i;
+        return 0;
+    });
 };
