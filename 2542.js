7612583d2e4bae148dc6a609706848aa40ad55a2
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-27 17:19:47 -0400

    Update 2542.js

--- 2542.js
+++ 2542.js
@@ -1,74 +1,56 @@
-class Heap {
-    constructor(isMin = true) {
-        this.isMin = isMin;
+class MinHeap {
+    constructor() {
         this.heap = [];
     }
-
-    size() {
-        return this.heap.length;
+    peek() {
+        return this.heap[0];
     }
-
     push(val) {
         this.heap.push(val);
         this.bubbleUp();
     }
-
-    peek() {
-        return this.heap[0];
-    }
-
     pop() {
-        const value = this.heap[0];
-        const bubbleValue = this.heap.pop();
+        const val = this.heap[0];
+        const ele = this.heap.pop();
         if (this.heap.length) {
-            this.heap[0] = bubbleValue;
+            this.heap[0] = ele;
             this.bubbleDown();
         }
-        return value;
+        return val;
     }
-
     bubbleUp() {
         let idx = this.heap.length - 1;
         while (idx >= 0) {
-            let lastAdded = this.heap[idx];
             let parentIdx = Math.floor((idx - 1) / 2);
-            let parentVal = parentIdx >= 0 ? this.heap[parentIdx] : 0;
-            if (
-                (this.isMin && lastAdded < parentVal) ||
-                (!this.isMin && lastAdded > parentVal)
-            ) {
-                [this.heap[parentIdx], this.heap[idx]] = [this.heap[idx], this.heap[parentIdx]];
-            }
-            idx = parentIdx;
+            if (this.heap[idx] < this.heap[parentIdx]) {
+                [this.heap[idx], this.heap[parentIdx]] = [this.heap[parentIdx], this.heap[idx]];
+                idx = parentIdx;
+            } else break;
         }
     }
-
     bubbleDown() {
         let idx = 0;
         while (idx < this.heap.length) {
-            let lastValue = this.heap[idx];
             let leftChildIdx = 2 * idx + 1;
             let rightChildIdx = 2 * idx + 2;
             let swapIdx;
-            if (leftChildIdx < this.heap.length) {
-                if (
-                    (this.isMin && lastValue > this.heap[leftChildIdx]) ||
-                    (!this.isMin && lastValue < this.heap[leftChildIdx])
-                ) {
-                    swapIdx = leftChildIdx;
-                }
+            if (
+                leftChildIdx < this.heap.length &&
+                this.heap[leftChildIdx] < this.heap[idx]
+            ) {
+                swapIdx = leftChildIdx;
             }
-            if (rightChildIdx < this.heap.length) {
-                let otherIdx = swapIdx ? swapIdx : idx;
-                if (
-                    (this.isMin && this.heap[otherIdx] > this.heap[rightChildIdx]) ||
-                    (!this.isMin && this.heap[otherIdx] < this.heap[rightChildIdx])
-                ) {
-                    swapIdx = rightChildIdx;
-                }
+            if (
+                rightChildIdx < this.heap.length &&
+                (
+                    (swapIdx && this.heap[rightChildIdx] < this.heap[swapIdx]) || 
+                    (!swapIdx && this.heap[rightChildIdx] < this.heap[idx])
+                ) 
+            ) {
+                swapIdx = rightChildIdx;
             }
             if (!swapIdx) break;
-            [this.heap[swapIdx], this.heap[idx]] = [this.heap[idx], this.heap[swapIdx]];
+            [this.heap[idx], this.heap[swapIdx]] = [this.heap[swapIdx], this.heap[idx]];
             idx = swapIdx;
         }
     }
@@ -81,24 +63,27 @@ class Heap {
  * @return {number}
  */
 var maxScore = function(nums1, nums2, k) {
-    const heap = new Heap();
-    let totalSum = 0;
-    let maxProduct = 0;
-    const indexes = nums1.map((_val, i) => i);
-    indexes.sort((a, b) => nums2[b] - nums2[a]);
-    for (const idx of indexes) {
-        const num1 = nums1[idx];
-        const num2 = nums2[idx];
-        // console.log(num1, num2, totalSum);
-        if (heap.size() === k) {
-            totalSum -= heap.pop();
+    const idxs = nums2.map((_num, idx) => idx)
+        .sort((a, b) => nums2[b] - nums2[a]);
+    let sum = 0;
+    let minHeapNum1 = new MinHeap();
+    for (let i = 0; i < k; i++) {
+        const num1 = nums1[idxs[i]];
+        sum += num1;
+        minHeapNum1.push(num1);
+    }
+    let maxProduct = nums2[idxs[k - 1]] * sum;
+    for (let i = k; i < idxs.length; i++) {
+        const num2 = nums2[idxs[i]];
+        let num1 = nums1[idxs[i]];
+        const removedNum1 = minHeapNum1.peek();
+        if (num1 > removedNum1) {
+            minHeapNum1.pop();
+            sum -= removedNum1;
+            sum += num1;
+            minHeapNum1.push(num1);
         }
-        totalSum += num1;
-        heap.push(num1);
-        // console.log(heap);
-        if (heap.size() === k) {
-            maxProduct = Math.max(maxProduct, totalSum * num2);
-        }
+        maxProduct = Math.max(maxProduct, num2 * sum);
     }
     return maxProduct;
 };
