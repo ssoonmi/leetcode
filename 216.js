33bef9c9989db9010b4f063f6dc63b7d77588506
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-28 17:52:18 -0400

    Update 216.js

--- 216.js
+++ 216.js
@@ -1,20 +1,26 @@
+/*
+1 - 9
+k numbers (at most 8 numbers)
+sum to n
+*/
+
 /**
  * @param {number} k
  * @param {number} n
  * @return {number[][]}
  */
-var combinationSum3 = function(k, n, min = 1) {
-    if (min > 9) return [];
-    if (n < 1) return [];
-    if (k === 1 && min === n) {
-        return [[min]];
+var combinationSum3 = function(k, n, num = 1, sum = 0, combination = '', ans = []) {
+    if (sum === n && k === combination.length) {
+        ans.push(combination.split(''));
+        return ans;
+    } else if (sum >= n || k <= combination.length) {
+        return ans;
+    } else if (num > 9) {
+        return ans;
     }
-    if (k <= 0) return [];
-    let resCombos = [];
-    const combos = combinationSum3(k - 1, n - min, min + 1);
-    for (const combo of combos) {
-        resCombos.push(combo.concat(min));
-    }
-    resCombos = resCombos.concat(combinationSum3(k, n, min + 1))
-    return resCombos;
+    combinationSum3(k, n, num + 1, sum, combination, ans);
+    combination += num;
+    sum += num;
+    combinationSum3(k, n, num + 1, sum, combination, ans);
+    return ans;
 };
