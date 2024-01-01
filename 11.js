4c63247011cb6f4330bb4cf9f3008d1bc1cc275d
Soon-Mi Sugihara <ssoonmi@gmail.com>
2023-12-31 21:01:12 -0400

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,5 +1,22 @@
-//  0  1  2  3  4  5
-// [3, 5, 2, 1, 4, 2]
+/*
+heights of length n
+n vertical lines
+find two lines that form a container with the most water
+
+return the maximum amount of water a container can store
+2D container - maximum area between two vertical lines determined by the x-axis
+
+can I assume that the input is going to be an array of positive heights?
+will there be any negative heights to consider?
+  |
+  |   | |
+| | | | | |
+0 1 2 3 4 5
+  1       2
+
+0 - 5 => min height is 1 => area is 5
+1 - 4 => min height is 2 => area is 8
+*/
 
 /**
  * @param {number[]} height
@@ -8,15 +25,17 @@
 var maxArea = function(height) {
     let i = 0;
     let j = height.length - 1;
-    let maxArea = 0;
+    let res = 0;
     while (i < j) {
-        const area = Math.min(height[j], height[i]) * (j - i);
-        maxArea = Math.max(maxArea, area);
-        if (height[j] < height[i]) {
-            j--;
-        } else {
+        const h1 = height[i];
+        const h2 = height[j];
+        const area = (j - i) * Math.min(h1, h2);
+        res = Math.max(res, area);
+        if (h1 <= h2) {
             i++;
+        } else {
+            j--;
         }
     }
-    return maxArea;
+    return res;
 };
