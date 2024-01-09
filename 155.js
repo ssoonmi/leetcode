0e3c8e6525d9cc0ba670b4664b90901a5803cef9
Soon-Mi Sugihara <ssoonmi@gmail.com>
2024-01-08 22:08:22 -0400

    Create 155.js

---
+++ 155.js
@@ -0,0 +1,50 @@
+
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if (!this.minStack.length) this.minStack.push(val);
+    else if (this.minStack[this.minStack.length - 1] > val) {
+        this.minStack.push(val);
+    } else {
+        this.minStack.push(this.minStack[this.minStack.length - 1]);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.minStack.pop();
+    return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
