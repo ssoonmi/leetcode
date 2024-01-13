7d7925fe978f767085101d941fbacf69ef6bdf5c
Soon-Mi Sugihara <ssoonmi@gmail.com>
2024-01-12 23:18:45 -0400

    Update 739.js

--- 739.js
+++ 739.js
@@ -1,34 +1,43 @@
+/*
+input: temperatures over days
+output: array of the number of days you have to wait after that day to get a warmer temperature (if there is no future day that's possible, set it to 0)
+
+input:  [10  2   2   5   1   3   7]
+answer: [ 0  2   1   3   1   1   0]
+          i
+          0  2   1   3   1   1   0]
+
+stack = [10]
+7 > 3
+3 > 1
+1 > 5? no => pop off 1 from stack
+3 > 5? no => pop off 3 from stack
+7 > 5? yes
+5 > 2
+2 > 2? no => pop off 2 from stack
+2 > 5? yes
+2 > 10? no => pop off 2 from stack
+5 > 10? no => pop off 5 from stack
+7 > 10? no => pop off 7 from stack
+
+O(2n) => O(n) time complexity (at most popping from the stack will be total average O(n))
+O(n) space complexity (worst case size of stack)
+*/
+
 /**
  * @param {number[]} temperatures
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    // if (!temperatures.length) return [];
-    // const res = [];
-    // res[temperatures.length - 1] = 0;
-    // const stack = [temperatures.length - 1];
-    // for (let i = temperatures.length - 2; i >= 0; i--) {
-    //     const temp = temperatures[i];
-    //     let latestTempI = stack[stack.length - 1];
-    //     let numDays = 0;
-    //     while (stack.length && temp >= temperatures[latestTempI]) {
-    //         stack.pop();
-    //         latestTempI = stack[stack.length - 1];
-    //     }
-    //     if (stack.length) numDays = latestTempI - i;
-    //     stack.push(i);
-    //     res[i] = numDays;
-    // }
-    // return res;
-
-    const res = [];
     const stack = [];
-    for (let i = 0; i < temperatures.length; i++) {
-        if (res[i] === undefined) res[i] = 0;
-        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
-            const j = stack.pop();
-            res[j] = i - j;
+    const res = [];
+    for (let i = temperatures.length - 1; i >= 0; i--) {
+        const temp = temperatures[i];
+        while (stack.length && temperatures[stack[stack.length - 1]] <= temp) {
+            stack.pop();
         }
+        if (!stack.length) res[i] = 0;
+        else res[i] = stack[stack.length - 1] - i;
         stack.push(i);
     }
     return res;
