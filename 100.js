e1e32341d3dff0784d22aa1eb726ac6c81235698
Soon-Mi Sugihara <ssoonmi@gmail.com>
2024-01-21 15:18:53 -0400

    Create 100.js

---
+++ 100.js
@@ -0,0 +1,29 @@
+/*
+2:10pm-2:18pm
+do a dfs on both, and check if the node values are the same
+recursively call dfs on both at the same time
+if one of the nodes are null and the other node isn't then return false
+if both nodes are null return true
+if both nodes are not null and the nodes have different values, return false
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
