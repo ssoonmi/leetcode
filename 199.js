da8de6d91ffb50be5e170eb9da214d2e46f2fc36
Soon-Mi Sugihara <ssoonmi@gmail.com>
2024-01-21 16:49:54 -0400

    Update 199.js

--- 199.js
+++ 199.js
@@ -1,3 +1,20 @@
+/*
+3:41pm-3:48pm
+return the values of the node from top to bottom
+        1
+       /.\
+      2.  3
+     /.  /
+    4.   7
+     \
+      9
+[1, 3, 7, 9]
+find all the nodes at each level and take the right most node and at it to the result
+keep track of the level of each node in bfs search
+and overwrite the value at the index at the result when traversing the tree bfs left to right
+*/
+
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -11,14 +28,19 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    const queue = [[root, 0]];
     const res = [];
+    const queue = [root];
+    let level = 0;
     while (queue.length) {
-        const [root, i] = queue.shift();
-        if (!root) continue;
-        res[i] = root.val;
-        queue.push([root.left, i + 1]);
-        queue.push([root.right, i + 1]);
+        const numNodesInLevel = queue.length;
+        for (let i = 0; i < numNodesInLevel; i++) {
+            const node = queue.shift();
+            if (!node) continue;
+            res[level] = node.val;
+            queue.push(node.left);
+            queue.push(node.right);
+        }
+        level++;
     }
     return res;
 };
